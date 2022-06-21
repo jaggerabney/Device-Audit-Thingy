b9191102a3726ca4acc09f9ca0786adc30f886c1
Jagger Abney <jagger.abney@gmail.com>
2022-06-21 14:05:28 -0700

    added dependencies

--- demo/src/main/java/com/jaggerabney/deviceaudit/App.java
+++ demo/src/main/java/com/jaggerabney/deviceaudit/App.java
@@ -1,7 +1,18 @@
 package com.jaggerabney.deviceaudit;
 
+import java.io.File;
+import java.io.FileInputStream;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File file = new File("/res/frmInventory.xlsx");
+            FileInputStream fis = new FileInputStream(file);
+            Workbook workbook = new XSSFWorkbook(fis);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
