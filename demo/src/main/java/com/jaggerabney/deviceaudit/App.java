7c079e389f17f026c8992cd1f5b87fb46a4d349b
Jagger Abney <jagger.abney@gmail.com>
2022-06-22 07:57:12 -0700

    added loadWorkbook function

--- demo/src/main/java/com/jaggerabney/deviceaudit/App.java
+++ demo/src/main/java/com/jaggerabney/deviceaudit/App.java
@@ -2,17 +2,26 @@ package com.jaggerabney.deviceaudit;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class App {
     public static void main(String[] args) {
         try {
-            File file = new File("/res/frmInventory.xlsx");
-            FileInputStream fis = new FileInputStream(file);
-            Workbook workbook = new XSSFWorkbook(fis);
+            // load first sheet in frmInventory.xlsx
+            Sheet sheet = loadWorkbook("frmInventory.xlsx").getSheetAt(0);
+            System.out.println(sheet.getRow(0).getCell(0));
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    private static XSSFWorkbook loadWorkbook(String filename) throws IOException {
+        InputStream is = App.class.getClassLoader().getResourceAsStream(filename);
+        return new XSSFWorkbook(is);
     }
 }
