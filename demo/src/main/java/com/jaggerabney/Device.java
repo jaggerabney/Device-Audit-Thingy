beecf1235eccbbe4b0e4aa4b297a9d41e5e56cda
Jagger Abney <jagger.abney@gmail.com>
2022-06-22 10:56:01 -0700

    created simple Device object

---
+++ demo/src/main/java/com/jaggerabney/Device.java
@@ -0,0 +1,10 @@
+package com.jaggerabney;
+
+public class Device {
+    // look i know making fields public is a bad idea but i dont want to use a
+    // million getters okay
+    public String asset;
+    public String serial;
+    public String model;
+    public String status;
+}
