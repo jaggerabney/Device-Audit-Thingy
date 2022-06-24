6ca4ac8698c46621c9d51370a1c67c04d95b12d6
Jagger Abney <jagger.abney@gmail.com>
2022-06-24 12:24:42 -0700

    added terminal updates and log4j (right this time)

--- demo/src/main/java/com/jaggerabney/deviceaudit/App.java
+++ demo/src/main/java/com/jaggerabney/deviceaudit/App.java
@@ -1,25 +1,33 @@
 package com.jaggerabney.deviceaudit;
 
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 
 public class App {
     public static final int NUM_DEVICE_PROPS = 7;
+    public static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.00");
 
     public static void main(String[] args) {
         try {
+            System.out.print("Loading audit.xlsx...");
             Workbook audit = loadWorkbook("audit.xlsx");
+            System.out.print("done!\nLoading inventory.xlsx...");
             Workbook inventory = loadWorkbook("inventory.xlsx");
+            System.out.print("done!\nLoading target.xlsx...");
             Workbook target = loadWorkbook("target.xlsx");
+            System.out.print("done!\n");
             Device[] devices = createDevicesFromAuditWorkbook(audit);
 
             devices = updateDevicesWithInventoryInfo(inventory, devices);
             target = updateTargetWorkbookWithDeviceInfo(target, devices);
 
+            System.out.print("\nWriting to target.xlsx...");
             OutputStream os = new FileOutputStream("target.xlsx");
             target.write(os);
+            System.out.print("done!");
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -84,11 +92,26 @@ public class App {
         ArrayList<Device> tempDevices = new ArrayList<>();
         int numSheets = workbook.getNumberOfSheets();
         int roomColIndex = 0, assetColIndex = 1, cotColIndex = 2;
+        double totalDevices = 0, numDevicesCreated = 0;
         Cell currentRoom, currentAsset, currentCot, lastRoom, lastCot;
         String room, asset, cot;
         Sheet currentSheet = null;
         Row currentRow = null;
         DataFormatter df = new DataFormatter();
+
+        for (int sheet = 0; sheet < numSheets; sheet++) {
+            currentSheet = workbook.getSheetAt(sheet);
+
+            for (int row = 0; row < currentSheet.getPhysicalNumberOfRows(); row++) {
+                if (currentSheet.getRow(row) != null && currentSheet.getRow(row).getCell(assetColIndex) != null) {
+                    totalDevices++;
+                }
+            }
+        }
+
+        System.out.print(
+                "Creating devices from audit workbook: " + (int) numDevicesCreated + " / " + (int) totalDevices + "("
+                        + DECIMAL_FORMAT.format(((numDevicesCreated / totalDevices) * 100)) + "%)");
 
         for (int sheet = 0; sheet < numSheets; sheet++) {
             currentSheet = workbook.getSheetAt(sheet);
@@ -115,6 +138,11 @@ public class App {
                     lastRoom = currentRoom;
                     lastCot = currentCot;
                     tempDevices.add(new Device(row, room, asset, cot));
+                    numDevicesCreated++;
+                    System.out.print("\rCreating devices from audit workbook: " + (int) numDevicesCreated + " / "
+                            + (int) totalDevices + " ("
+                            + DECIMAL_FORMAT.format(((numDevicesCreated / totalDevices) * 100))
+                            + "%)");
                 }
             }
         }
@@ -128,11 +156,16 @@ public class App {
                 serialColIndex = getIndexOfColumn(sheet, "SerialNum"),
                 modelColIndex = getIndexOfColumn(sheet, "ItemDesc"),
                 statusColIndex = getIndexOfColumn(sheet, "Status");
+        double numDevicesTotal = devices.length, numDevicesUpdated = 0;
         String currentSerial = "", currentModel = "", currentStatus = "";
         List<String> assetTags = Arrays.asList(getValuesOfColumn(sheet, assetColIndex));
         ArrayList<Device> result = new ArrayList<>();
         DataFormatter df = new DataFormatter();
         int currentRow = 0;
+
+        System.out.print("\nUpdating devices with info from inventory.xlsx: " + (int) numDevicesUpdated + " / "
+                + (int) numDevicesTotal + " (" + DECIMAL_FORMAT.format(((numDevicesUpdated / numDevicesTotal) * 100))
+                + "%)");
 
         for (Device device : devices) {
             currentRow = assetTags.indexOf(device.asset);
@@ -146,6 +179,13 @@ public class App {
                         new Device(currentRow, device.asset, currentSerial, device.location, device.room, currentModel,
                                 device.cot, currentStatus));
             }
+
+            numDevicesUpdated++;
+            System.out.print("\rUpdating devices with info from inventory.xlsx: " + (int) numDevicesUpdated + " / "
+                    + (int) numDevicesTotal + " ("
+                    + DECIMAL_FORMAT.format(((numDevicesUpdated / numDevicesTotal) * 100))
+                    + "%)");
+
         }
 
         return result.toArray(new Device[0]);
@@ -163,6 +203,10 @@ public class App {
                 modelColIndex = getIndexOfColumn(sheet, "Model Name"),
                 cotColIndex = getIndexOfColumn(sheet, "Checked Out to"),
                 statusColIndex = getIndexOfColumn(sheet, "Status");
+        double numRowsTotal = devices.length - 1, numRowsUpdated = 0; // -1 because both vars are 0-indexed
+
+        System.out.print("\nUpdating target.xlsx with device info: " + (int) numRowsUpdated + " / " + (int) numRowsTotal
+                + " (" + DECIMAL_FORMAT.format(((numRowsUpdated / numRowsTotal) * 100)) + "%)");
 
         for (int row = 1; row < devices.length; row++) {
             if (sheet.getRow(row) == null) {
@@ -182,6 +226,11 @@ public class App {
             currentRow.getCell(modelColIndex).setCellValue(currentDevice.model);
             currentRow.getCell(cotColIndex).setCellValue(currentDevice.cot);
             currentRow.getCell(statusColIndex).setCellValue(currentDevice.status);
+
+            numRowsUpdated++;
+            System.out.print(
+                    "\rUpdating target.xlsx with device info: " + (int) numRowsUpdated + " / " + (int) numRowsTotal
+                            + " (" + DECIMAL_FORMAT.format(((numRowsUpdated / numRowsTotal) * 100)) + "%)");
         }
 
         return result;
